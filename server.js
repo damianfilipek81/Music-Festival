08b3be5230751ba637a496efe49cc022ea1508e5
Damian <damianfilipek81@gmail.com>
2021-05-31 19:48:55 +0200

    Connect client and server to socket

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const path = require('path');
+const socket = require('socket.io');
 
 const testimonialsRoutes = require('./routes/testimonials.routes.js');
 const concertsRoutes = require('./routes/concerts.routes.js');
@@ -24,6 +25,13 @@ app.use((req, res) => {
   res.status(404).json({ message: 'Not found...' })
 });
 
-app.listen(process.env.PORT || 8000, () => {
+const server = app.listen(process.env.PORT || 8000, () => {
   console.log('Server is running on port: 8000');
 });
+
+const io = socket(server, { cors: { origin: '*', } });
+
+io.on('connection', (socket) => {
+  console.log('New socket!');
+})
+
